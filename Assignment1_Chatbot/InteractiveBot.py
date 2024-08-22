b26a44626562e23d5c91e511faaeb8e375955827
NaphatNu <tonklanapat@gmail.com>
2024-08-22 11:41:30 +0700

    Feat : quick_reply

--- Assignment1_Chatbot/InteractiveBot.py
+++ Assignment1_Chatbot/InteractiveBot.py
@@ -17,10 +17,6 @@ def loadInformationFile(file_path: str):
         data: dict = json.load(file) # load data จาก json
     return data
 
-def saveInformation(file_path: str, data: str):
-    with open(file_path, 'w') as file:
-        json.dump(data, file, indent=2) # เขียนข้อมูลลง json
-
 def findMatch(user_question: str, question: list[str]) -> Optional[str]:
     matches: list = get_close_matches(user_question, question, n=1, cutoff=0.6)
     return matches[0] if matches else None
@@ -42,6 +38,41 @@ def reply_msg(array_header, array_post_data):
     response = requests.post(url, headers=array_header, json=array_post_data)
     return response.json()
 
+def push_msg(array_header, array_post_data):
+    url = "https://api.line.me/v2/bot/message/push"
+    response = requests.post(url, headers=array_header, json=array_post_data)
+    return response.json()
+    
+def quick_reply_list():
+    knowledge: dict = loadInformationFile('Assignment1_Chatbot/Information.json')
+    actions = []
+    for idx,question in enumerate(knowledge['faqs']):
+        if question["label"] == "skip":
+            continue
+        elif idx == 13:
+            action = {
+            "type": "action",
+            "action": {
+                "type": "message",
+                "label": "ต่อไป",
+                "text": "ต่อไป"
+                }
+            }
+            actions.append(action)
+        else:
+            action = {
+            "type": "action",
+            "action": {
+                "type": "message",
+                "label": question["label"],
+                "text": question["question"]
+                }
+            }
+            actions.append(action)
+    
+        # print(actions)
+    return actions
+
 def ChatBot(User_Input,content):
     knowledge: dict = loadInformationFile('Assignment1_Chatbot/Information.json')
     user_input: str = User_Input # type: ignore
@@ -49,9 +80,11 @@ def ChatBot(User_Input,content):
     reply_token = content['events'][0]['replyToken']
     userId = content['events'][0]['source']['userId']
 
-    best_match: str | None = findMatch(user_input, [q["question"] for q in knowledge["faqs"]])
-    if best_match:
-        answer: str = getAnswer(best_match, knowledge) # type: ignore
+    question_match: str | None = findMatch(user_input, [q["question"] for q in knowledge["faqs"]])
+
+
+    if question_match:
+        answer: str = getAnswer(question_match, knowledge) # type: ignore
         print(f"Bot : {answer}")
 
         array_post_data = {
@@ -63,32 +96,38 @@ def ChatBot(User_Input,content):
             }
             ]
         }
-    
 
     elif user_input == "ตัวเลือก":
+        quick_reply_list_1 = quick_reply_list()[0:13]
+            
         array_post_data = {
-  "to": "Ub47e3bcd9779b4f736b3e4db6d401926",
-  "messages": [
-    {
-      "type": "text",
-      "text": "Hello Quick Reply!",
-      "quickReply": {
-        "items": [
-         
-          {
-            "type": "action",
-            "action": {
-              "type": "message",
-              "label": "ปรัชญาของภาควิชา",
-              "text": "ปรัชญาของภาควิชาวิศวกรรมคอมพิวเตอร์ มศว คืออะไร?"
+            "to": userId,
+            "messages": [
+                {
+                "type": "text",
+                "text": "ตัวเลือก",
+                "quickReply": {
+                    "items": quick_reply_list_1
+                }
+                }
+            ]
             }
+    
+    elif user_input == "ต่อไป":
+        quick_reply_list_2 = quick_reply_list()[14:]
+            
+        array_post_data = {
+            "to": userId,
+            "messages": [
+                {
+                "type": "text",
+                "text": "ตัวเลือก2",
+                "quickReply": {
+                    "items": quick_reply_list_2
+                }
+                }
+            ]
             }
-
-        ]
-      }
-    }
-   ]
-}
 
     return array_post_data
 
@@ -102,13 +141,22 @@ def webhook():
 
     message = content['events'][0]['message']['text']
 
-    print("message:",message)
-    print(content)
-
     array_post_data = ChatBot(message,content)
     # print(array_post_data)
-    reply_msg(array_header, array_post_data)
-    return "OK", 200
+    # print("message:",message)
+    # print(content)
+
+    if message == "ตัวเลือก":
+        push_msg(array_header, array_post_data)
+        return "OK", 200
+    
+    elif message == "ต่อไป":
+        push_msg(array_header, array_post_data)
+        return "OK", 200
+    
+    else:
+        reply_msg(array_header, array_post_data)
+        return "OK", 200
 
 if __name__ == '__main__':
     app.run(port=8080)
